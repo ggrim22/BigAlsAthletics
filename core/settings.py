fc8e725fa33ec462fc42729972a4e166bf3df467
ggrim22 <gtgrim22@gmail.com>
2025-10-12 17:55:13 -0400

    add files for deployment

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
+import django_heroku
 from django.contrib import messages
 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--o3xzkmxen0-aie7p734jgn4+cc8qg__86pl90-e0&n0yeacw3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-key")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 
@@ -47,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,10 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -122,13 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -158,3 +156,11 @@ MESSAGE_TAGS = {
     messages.WARNING: "text-dark bg-warning",
     messages.ERROR: "text-white bg-danger",
 }
+
+CSP_DEFAULT_SRC = (os.getenv('CSP_DEFAULT_SRC', "'none'"),)
+CSP_SCRIPT_SRC = tuple(os.getenv('CSP_SCRIPT_SRC', "'self'").split())
+CSP_STYLE_SRC = tuple(os.getenv('CSP_STYLE_SRC', "'self'").split())
+CSP_FONT_SRC = tuple(os.getenv('CSP_FONT_SRC', "'self'").split())
+CSP_IMG_SRC = tuple(os.getenv('CSP_IMG_SRC', "'self'").split())
+CSP_CONNECT_SRC = tuple(os.getenv('CSP_CONNECT_SRC', "'self'").split())
+CSP_FRAME_SRC = tuple(os.getenv('CSP_FRAME_SRC', "'self'").split())
