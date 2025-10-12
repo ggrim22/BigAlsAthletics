0f4630200fd81366d4bca3ce5fead349e7047d63
ggrim22 <gtgrim22@gmail.com>
2025-10-12 18:19:49 -0400

    Fix settings

--- core/settings.py
+++ core/settings.py
@@ -85,14 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'big-als-athletics',
-        'USER': 'postgres',
-        'PASSWORD': 'Mistsoup1',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
