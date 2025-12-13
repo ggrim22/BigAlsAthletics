12337e328e5c6138370dce0c08fe89edfc9f208d
ggrim22 <gtgrim22@gmail.com>
2025-12-13 14:36:13 -0500

    Fix download

--- static/js/download.js
+++ static/js/download.js
@@ -1,11 +1,24 @@
-  document.getElementById("filter-form").addEventListener("change", function () {
-      const selectedCollection = this.querySelector("select").value;
-      const downloadBtn = document.getElementById("download-btn");
-      const baseUrl = downloadBtn.getAttribute("href").split("?")[0];
+document.getElementById("filter-form").addEventListener("change", function () {
+    const downloadBtn = document.getElementById("download-btn");
+    const baseUrl = downloadBtn.getAttribute("href").split("?")[0];
 
-      if (selectedCollection) {
-          downloadBtn.setAttribute("href", `${baseUrl}?collection=${selectedCollection}`);
-      } else {
-          downloadBtn.setAttribute("href", baseUrl);
-      }
-  });
+    const collectionSelect = this.querySelector("select[name='collection']");
+    const productNameSelect = this.querySelector("select[name='product_name']");
+
+    const selectedCollection = collectionSelect ? collectionSelect.value : "";
+    const selectedProductName = productNameSelect ? productNameSelect.value : "";
+
+    const params = new URLSearchParams();
+    if (selectedCollection) {
+        params.append("collection", selectedCollection);
+    }
+    if (selectedProductName) {
+        params.append("product_name", selectedProductName);
+    }
+
+    if (params.toString()) {
+        downloadBtn.setAttribute("href", `${baseUrl}?${params.toString()}`);
+    } else {
+        downloadBtn.setAttribute("href", baseUrl);
+    }
+});
